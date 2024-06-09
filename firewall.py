2797e90fd70cfbaa4a42919d2fdc340d2171db79
josinger <josinger@fi.uba.ar>
2024-06-08 23:02:14 -0300

    firewall more fixes

--- firewall.py
+++ firewall.py
@@ -24,9 +24,9 @@ class Controller(EventMixin):
             #Rule 1
             rule1 = of.ofp_flow_mod()
             rule1.match.tp_dst = 80
-            rule1.match.nw_proto = ipv4.TCP_PROTOCOL
+            rule1.match.nw_proto = ipv4.UDP_PROTOCOL
             rule1.match.dl_type = ethernet.IP_TYPE
-            rule1.actions.append(of.ofp_action_output(port = of.OFPP_FLOOD))
+            #rule1.actions.append(of.ofp_action_output(port = of.OFPP_NONE))
             event.connection.send(rule1)
             #Rule 2
             rule2 = of.ofp_flow_mod()
@@ -34,7 +34,7 @@ class Controller(EventMixin):
             rule2.match.nw_proto = ipv4.UDP_PROTOCOL
             rule2.match.dl_type = ethernet.IP_TYPE
             rule2.match.nw_src = IPAddr("10.0.0.1")
-            rule2.actions.append(of.ofp_action_output(port = of.OFPP_FLOOD))
+            #rule2.actions.append(of.ofp_action_output(port = of.OFPP_NONE))
             event.connection.send(rule2)
             #Rule 3 
             rule31 = of.ofp_flow_mod()
